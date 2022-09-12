4e79a0e091e56ceca77deb94de76bc2910c8198c
Luca Lattore <luca.lattore@waveinformatica.com>
2022-09-12 17:22:35 +0200

    json include directives

--- src/main/java/com/waveinformatica/demo/dto/MarketDTO.java
+++ src/main/java/com/waveinformatica/demo/dto/MarketDTO.java
@@ -1,8 +1,18 @@
 package com.waveinformatica.demo.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class MarketDTO {
     private long id;
     private String name;
+    private String area;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private List<String> brands = new ArrayList<>();
 
     public long getId() {
         return id;
@@ -18,5 +28,17 @@ public class MarketDTO {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public List<String> getBrands() {
+        return brands;
     }
 }
